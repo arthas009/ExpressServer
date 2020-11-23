24f25132d0ab2365b9b26a45d066f604770d07c4
Yusuf <yusufalti1997@gmail.com>
2020-11-23 08:11:22 +0300

    .

--- routes/index.js
+++ routes/index.js
@@ -118,7 +118,7 @@ router.get('/madalyalarfotolarinigetir', function (req, res, next) {
 
 });
 
-router.get('/madalyalarfotolarinigetir', function (req, res, next) {
+router.get('/anasayfafotolarinigetir', function (req, res, next) {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE'); // If needed
     res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type'); // If needed
